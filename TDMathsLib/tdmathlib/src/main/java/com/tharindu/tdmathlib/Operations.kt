6bfa80e2ec0af354db4fc02413376776962a8cb2
Tharindu <tharindu@jrcsoftware.com>
2019-06-20 14:13:59 +0530

    Initial commit

---
+++ TDMathsLib/tdmathlib/src/main/java/com/tharindu/tdmathlib/Operations.kt
@@ -0,0 +1,8 @@
+package com.tharindu.tdmathlib
+
+object Operations {
+    fun sum(a: Int, b: Int) = a + b
+    fun minus(a: Int, b: Int) = a - b
+    fun mulitply(a: Int, b: Int) = a * b
+    fun devide(a: Int, b: Int) = a / b
+}
