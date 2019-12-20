02a2bac8b69c686efb8bd6305d74304e6f0a5e30
Tharindu <tharindu@jrcsoftware.com>
2019-12-20 20:50:07 +0530

    Some changes

--- TDMathsLib/app/src/main/java/com/tharindu/tdmathslib/MainActivity.kt
+++ TDMathsLib/app/src/main/java/com/tharindu/tdmathslib/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.tharindu.tdmathslib
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.tharindu.tdmathlib.TestLibraryActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, TestLibraryActivity::class.java))
     }
 }
